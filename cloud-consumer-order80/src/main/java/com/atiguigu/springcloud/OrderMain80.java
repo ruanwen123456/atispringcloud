354be64da74247ad5ca903bd31fa477f836734ae
ruanwen123456 <1300945176@qq.com>
2022-03-06 11:25:56 +0800

    cloud 单机eureka

--- cloud-consumer-order80/src/main/java/com/atiguigu/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/atiguigu/springcloud/OrderMain80.java
@@ -4,11 +4,13 @@ package com.atiguigu.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan
+@EnableEurekaClient
 public class OrderMain80 {
 
     public static void main(String[] args) {
--- cloud-eureka/src/main/java/com/atguigu/springcloud/EurekaMain.java
+++ cloud-eureka/src/main/java/com/atguigu/springcloud/EurekaMain.java
@@ -1,19 +1,20 @@
 package com.atguigu.springcloud;
 
 
-import org.apache.ibatis.annotations.Mapper;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentCloudMain {
+@EnableAutoConfiguration
+@ComponentScan
+@EnableEurekaServer
+public class EurekaMain {
 
     public static void main(String[] args) {
-        SpringApplication.run(PaymentCloudMain.class);
+        SpringApplication.run(EurekaMain.class,args);
     }
 }
+
